c6969cc9263baf939948d8b889756a91e1784716
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-26 12:53:02 +0300

    secreate key

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '008a4faa5e795f9709bfe2c557a598c57f2af14e5933ff06'
 
 messages = [{'title': 'Message One',
              'content': 'Message One Content'},
@@ -18,4 +19,16 @@ def about():
 
 @app.route('/create/', methods=('GET', 'POST'))
 def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            messages.append({'title': title, 'content': content})
+            return redirect(url_for('index'))
+    
     return render_template('create.html')
